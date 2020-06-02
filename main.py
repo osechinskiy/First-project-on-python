a294c4f807646cf092c9ab53fb6b8bc5431c7608
osechinskiy <osechinskiy@gmail.com>
2020-06-02 21:23:04 +0300

    Добавлен цикл

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 print("Hello, it's my first program on python")
-numberone = int(input("Enter first number"))
-numertwo = int(input("Enter second number"))
-choice = input("What the action to perform? (+, -, *, /)")
+i = "y"
+while i == "y":
 
-if choice == "+":
-    print("Answer = ", numberone + numertwo)
-elif choice == "-":
-    print("Answer = ", numberone - numertwo)
-elif choice == "*":
-    print("Answer = ", numberone * numertwo)
-elif choice == "/":
-    if numberone == 0:
-        print("Error, it is impossible to fulfill the request, it is impossible to divide by zero ")
-    else:
-        ("Answer = ", numberone / numertwo)
+    numberone = int(input("Enter first number: "))
+    numertwo = int(input("Enter second number: "))
+    choice = input("What the action to perform? (+, -, *, /): ")
+
+    if choice == "+":
+        print("Answer = ", numberone + numertwo)
+    elif choice == "-":
+        print("Answer = ", numberone - numertwo)
+    elif choice == "*":
+        print("Answer = ", numberone * numertwo)
+    elif choice == "/":
+        if numberone <= 0:
+            print("Error, it is impossible to fulfill the request, it is impossible to divide by zero ")
+        else:
+            ("Answer = ", numberone / numertwo)
+
+    i = input("Do you want to refresh? y/n: ")
